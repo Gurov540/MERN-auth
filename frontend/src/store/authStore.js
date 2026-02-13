02e7cb0a581d532cb486e4c8c839bc3ed9fd1cfe
Gurov540 <70337278+Gurov540@users.noreply.github.com>
2026-02-13 21:58:26 +0300

    create basic auth store

---
+++ frontend/src/store/authStore.js
@@ -0,0 +1,18 @@
+import { creeate } from "zustand";
+import axios from "axios";
+
+const API_URL =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:5000/api/auth"
+    : "/api/auth";
+
+axios.defaults.withCredentials = true;
+
+export const useAuthStore = create((set) => ({
+  user: null,
+  isAuthenticated: false,
+  error: null,
+  isLoading: false,
+  isCheckingAuth: true,
+  message: null,
+}));
