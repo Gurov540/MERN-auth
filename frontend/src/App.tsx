c282510a849eaa21b084dbf31efa1c5c18ee4fd1
Gurov540 <70337278+Gurov540@users.noreply.github.com>
2026-02-25 20:58:19 +0300

    add routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,15 @@
-import "./App.css";
-
-import { Button } from "./shared/ui/Button";
-import { Input } from "./shared/ui/Input";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage/HomePage";
+import LoginPage from "./pages/SignInPage/SignInPage";
+import SignUpPage from "./pages/SignUpPage/SignUpPage";
 
 function App() {
   return (
-    <>
-      <Button type="submit">Регистрация</Button>
-      <Input type="email" placeholder="Login" fullWidth></Input>
-    </>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/signin" element={<LoginPage />} />
+      <Route path="/signup" element={<SignUpPage />} />
+    </Routes>
   );
 }
 
