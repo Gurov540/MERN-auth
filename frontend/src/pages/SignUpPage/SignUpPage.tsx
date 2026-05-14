733e5a95ed710952abbbc06f6dfccc197f91eb95
Gurov540 <70337278+Gurov540@users.noreply.github.com>
2026-05-14 21:17:36 +0300

    change input to password input

--- frontend/src/pages/SignUpPage/SignUpPage.tsx
+++ frontend/src/pages/SignUpPage/SignUpPage.tsx
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom";
 import styles from "./SignUpPage.module.css";
 
 import { Input } from "../../shared/ui/Input";
+import { PasswordInput } from "../../shared/ui/Input/PasswordInput";
 import { Button } from "../../shared/ui/Button";
 
 const SignUpPage = () => {
@@ -11,9 +12,13 @@ const SignUpPage = () => {
         <h1 className={styles.formHeader}>Register form</h1>
 
         <div className={styles.inputGroup}>
-          <Input type="email" placeholder="Email address" />
-          <Input type="password" placeholder="Password" />
-          <Input type="password" placeholder="Enter password" />
+          <Input type="email" placeholder="Email address" fullWidth={true} />
+          <PasswordInput
+            placeholder="Password"
+            hint="Используйте буквы, цифры и символы"
+            fullWidth={true}
+          />
+          <PasswordInput placeholder="Enter Password" fullWidth={true} />
         </div>
 
         <Button type="submit" buttonSize="large">
