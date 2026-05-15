972d57f84504efa3352897df9c3da1683a6328c3
Gurov540 <70337278+Gurov540@users.noreply.github.com>
2026-05-15 22:11:28 +0300

    add dashboard route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,9 @@
 import { Routes, Route } from "react-router-dom";
+import { ProtectedRoute } from "./components/ProtectedRoute";
 import HomePage from "./pages/HomePage/HomePage";
 import LoginPage from "./pages/SignInPage/SignInPage";
 import SignUpPage from "./pages/SignUpPage/SignUpPage";
+import DashboardPage from "./pages/Dashboard/Dashboard";
 
 function App() {
   return (
@@ -9,6 +11,14 @@ function App() {
       <Route path="/" element={<HomePage />} />
       <Route path="/signin" element={<LoginPage />} />
       <Route path="/signup" element={<SignUpPage />} />
+      <Route
+        path="/dashboard"
+        element={
+          <ProtectedRoute>
+            <DashboardPage />
+          </ProtectedRoute>
+        }
+      />
     </Routes>
   );
 }
